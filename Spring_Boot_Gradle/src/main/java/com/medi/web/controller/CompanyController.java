da465dbbbbdbdc092470e2bf0fce2f71fe78027c
sharn <sharn@DESKTOP-09H25EH>
2018-10-26 20:04:56 +0530

    Changes as per Login Model flow - Sharnendra Dey

--- Spring_Boot_Gradle/src/main/java/com/medi/web/controller/CompanyController.java
+++ Spring_Boot_Gradle/src/main/java/com/medi/web/controller/CompanyController.java
@@ -1,17 +1,20 @@
 package com.medi.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medi.web.model.ErrorResponse;
 import com.medi.web.model.MedicineCompanyMaster;
 import com.medi.web.service.CompanyService;
+import com.medi.web.view.CompanyInfoView;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -24,6 +27,7 @@ public class CompanyController {
 	@Autowired
 	private CompanyService companyService;
 	
+
 	@ApiOperation(nickname = "retrieveRequests", value = "This API is retrieve review requests against a particular reviewer id", response = String.class)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = String.class),
 			@ApiResponse(code = 400, message = "Bad Request", response = ErrorResponse.class),
@@ -31,18 +35,12 @@ public class CompanyController {
 			@ApiResponse(code = 500, message = "Internal Server Error (Server Error)", response = ErrorResponse.class),
 			@ApiResponse(code = 503, message = "Service Unavailable", response = ErrorResponse.class) })
 	
-	
-	/*
-	 * This function will take the new model object set the id from the previous
-	 * object and then persist the changes. 
-	 * 
-	 * */ 
-	
-	@GetMapping(value = "/addCompany", produces = { "application/json;charset=UTF-8" })
-	public ResponseEntity<MedicineCompanyMaster> addCompany(HttpServletRequest httpServletRequest) throws Exception {
-		MedicineCompanyMaster mc=new MedicineCompanyMaster();
-		companyService.insertCompany(mc);
-		return ResponseEntity.ok().body(mc);
+	@PostMapping(value = "/addCompany", produces = { "application/json;charset=UTF-8" })
+	public ResponseEntity<MedicineCompanyMaster> addCompany(@RequestBody final CompanyInfoView companyInfoView,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception {
+		final MedicineCompanyMaster medicineCompanyMaster=new MedicineCompanyMaster();
+		BeanUtils.copyProperties(companyInfoView, medicineCompanyMaster);
+		companyService.insertCompany(medicineCompanyMaster);
+		return ResponseEntity.ok().body(medicineCompanyMaster);
 	}
 	
 	
@@ -52,12 +50,18 @@ public class CompanyController {
 	 * object and then persist the changes. 
 	 * 
 	 * */
-	
+
+	@ApiOperation(nickname = "retrieveRequests", value = "This API is retrieve review requests against a particular reviewer id", response = String.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = String.class),
+			@ApiResponse(code = 400, message = "Bad Request", response = ErrorResponse.class),
+			@ApiResponse(code = 404, message = "Not Found", response = ErrorResponse.class),
+			@ApiResponse(code = 500, message = "Internal Server Error (Server Error)", response = ErrorResponse.class),
+			@ApiResponse(code = 503, message = "Service Unavailable", response = ErrorResponse.class) })
 	
 	@PostMapping(value = "/getCompany", produces = { "application/json;charset=UTF-8" })
-	public ResponseEntity<MedicineCompanyMaster> getCompany(HttpServletRequest httpServletRequest) throws Exception {
-		String st="Sample Company";
-		return ResponseEntity.ok().body(companyService.getCompany(st));
+	public ResponseEntity<MedicineCompanyMaster> getCompany(@RequestBody final String companyViewName,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception {
+		final MedicineCompanyMaster medicineCompanyMaster=companyService.getCompany(companyViewName);
+		return ResponseEntity.ok().body(medicineCompanyMaster);
 	}
 	
 	
@@ -67,17 +71,21 @@ public class CompanyController {
 	 * 
 	 * */
 	
+	@ApiOperation(nickname = "retrieveRequests", value = "This API is retrieve review requests against a particular reviewer id", response = String.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = String.class),
+			@ApiResponse(code = 400, message = "Bad Request", response = ErrorResponse.class),
+			@ApiResponse(code = 404, message = "Not Found", response = ErrorResponse.class),
+			@ApiResponse(code = 500, message = "Internal Server Error (Server Error)", response = ErrorResponse.class),
+			@ApiResponse(code = 503, message = "Service Unavailable", response = ErrorResponse.class) })
+	//this is to be tested without id it is very important
 	
-	@PostMapping(value = "/editCompany", produces = { "application/json;charset=UTF-8" })
-	public ResponseEntity<String> editCompany(HttpServletRequest httpServletRequest) throws Exception {
-		MedicineCompanyMaster sampleMedicineCompanyMaster=new MedicineCompanyMaster();
+	@PostMapping(value = "/saveCompany", produces = { "application/json;charset=UTF-8" })
+	public ResponseEntity<String> editCompany(@RequestBody final CompanyInfoView companyInfoView,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception {
 		
-		final MedicineCompanyMaster medicineCompanyMaster;
-		medicineCompanyMaster=companyService.getCompany(sampleMedicineCompanyMaster.getCompanyName());
-		
-		sampleMedicineCompanyMaster.setMedicineId(medicineCompanyMaster.getMedicineId());
-		companyService.insertCompany(sampleMedicineCompanyMaster);
-		return ResponseEntity.ok().body(sampleMedicineCompanyMaster.getCompanyName()+" is Edited");
+		final MedicineCompanyMaster medicineCompanyMaster=new MedicineCompanyMaster();
+		BeanUtils.copyProperties(companyInfoView, medicineCompanyMaster);
+		companyService.insertCompany(medicineCompanyMaster);
+		return ResponseEntity.ok().body(medicineCompanyMaster.getCompanyName()+" is Saved");
 	}
 	
 
---
+++ Spring_Boot_Gradle/src/main/java/com/medi/web/view/CompanyInfoView.java
@@ -0,0 +1,44 @@
+package com.medi.web.view;
+
+import java.util.Date;
+
+public class CompanyInfoView {
+	
+	private int medicineId;
+	private String medicineName;
+	private String companyName;
+	private Date createdDate;
+	private Date modifiedDate;
+	
+	public int getMedicineId() {
+		return medicineId;
+	}
+	public void setMedicineId(int medicineId) {
+		this.medicineId = medicineId;
+	}
+	public String getMedicineName() {
+		return medicineName;
+	}
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+}
