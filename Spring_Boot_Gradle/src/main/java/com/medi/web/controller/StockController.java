72d2cf2aab30654b781d4f977055aba6643242fd
sharn <sharn@DESKTOP-09H25EH>
2018-11-18 00:41:13 +0530

    Changes to resolve Quantity class bug- Sharnendra Dey

--- Spring_Boot_Gradle/src/main/java/com/medi/web/controller/StockController.java
+++ Spring_Boot_Gradle/src/main/java/com/medi/web/controller/StockController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medi.web.model.ErrorResponse;
+import com.medi.web.model.Quantity;
 import com.medi.web.model.StockMaster;
 import com.medi.web.service.StockService;
 import com.medi.web.view.StockInfoView;
@@ -42,7 +43,12 @@ public class StockController {
 	@PostMapping(value = "/addStock", produces = { "application/json;charset=UTF-8" })
 	public ResponseEntity<StockMaster> addStock(@RequestBody final StockInfoView stockInfoView,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception {
 		final StockMaster stockMaster=new StockMaster();
+		final Quantity quantity=new Quantity();
 		BeanUtils.copyProperties(stockInfoView, stockMaster);
+		quantity.setAvailableQuantity(stockInfoView.getQuantity().getAvailableQuantity());
+		quantity.setCostPerQuantity(stockInfoView.getQuantity().getCostPerQuantity());
+		quantity.setTotalQuantity(stockInfoView.getQuantity().getTotalQuantity());
+		stockMaster.setQuantity(quantity);
 		stockService.addStock(stockMaster);
 		return ResponseEntity.ok().body(stockMaster);
 	}
